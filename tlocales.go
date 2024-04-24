43d13781b78462feec2581c03ddb00258d60e48a
RDMTSTUDIOS <rdmtstudios@yandex.ru>
2024-04-24 14:23:37 +0300

    ådded localization structs Book and Dictionary

--- tlocales.go
+++ tlocales.go
@@ -5,3 +5,9 @@ type LocalesObject struct {
 	Lang       string
 	Extension  string
 }
+
+// Holds dictionaries as values by their names as keys
+type Book map[string]Dictionary
+
+// Holds definitions by key and phrases by value
+type Dictionary map[string]string
